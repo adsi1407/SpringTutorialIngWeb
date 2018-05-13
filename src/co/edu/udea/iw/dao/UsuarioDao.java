ebaea0aa1f23a6d1ada3623b45fe25d033815ae5
David Santacoloma <andres.santacoloma@ceiba.com.co>
2018-05-13 17:49:23 -0500

    Implementar Dao para los usuarios

--- src/co/edu/udea/iw/dao/UsuarioDao.java
+++ src/co/edu/udea/iw/dao/UsuarioDao.java
@@ -5,8 +5,8 @@ import co.edu.udea.iw.dto.Usuario;
 import co.edu.udea.iw.exception.ClassException;
 
 public interface UsuarioDao {
-	public void insertar(Usuario usuario) throws ClassException;
-	public void actualizar(Usuario usuario) throws ClassException;
+	public Usuario insertar(Usuario usuario) throws ClassException;
+	public Usuario actualizar(Usuario usuario) throws ClassException;
 	public void eliminar(Usuario usuario) throws ClassException;
 	public List<Usuario> obtener() throws ClassException;
 	public Usuario obtenerUsuario(String login) throws ClassException;
--- src/co/edu/udea/iw/dao/impl/UsuarioDaoImpl.java
+++ src/co/edu/udea/iw/dao/impl/UsuarioDaoImpl.java
@@ -1,7 +1,12 @@
 package co.edu.udea.iw.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import co.edu.udea.iw.dao.UsuarioDao;
@@ -11,33 +16,108 @@ import co.edu.udea.iw.exception.ClassException;
 public class UsuarioDaoImpl extends HibernateDaoSupport implements UsuarioDao {
 
 	@Override
-	public void insertar(Usuario usuario) throws ClassException {
-		// TODO Auto-generated method stub
+	public Usuario insertar(Usuario usuario) throws ClassException {
 		
+		Transaction tx = null;
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            tx = session.beginTransaction();
+            session.save(usuario);
+            tx.commit();
+            
+            
+        }catch(HibernateException e){
+            throw new ClassException(e);
+        }finally{
+            session.close();
+        }
+        
+        return usuario;
 	}
 
 	@Override
-	public void actualizar(Usuario usuario) throws ClassException {
-		// TODO Auto-generated method stub
+	public Usuario actualizar(Usuario usuario) throws ClassException {
 		
+		Transaction tx = null;
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            tx = session.beginTransaction();
+            session.update(usuario);
+            tx.commit();
+            
+            
+        }catch(HibernateException e){
+            throw new ClassException(e);
+        }finally{
+            session.close();
+        }
+        
+        return usuario;
 	}
 
 	@Override
 	public void eliminar(Usuario usuario) throws ClassException {
-		// TODO Auto-generated method stub
 		
+		Transaction tx = null;
+		Session session = null;
+		
+		try {
+			
+			session = this.getSessionFactory().getCurrentSession();
+            
+            tx = session.beginTransaction();
+            session.delete(usuario);
+            tx.commit();
+            
+		}catch(HibernateException e){
+            throw new ClassException(e);
+        }finally{
+            session.close();
+        }
 	}
 
 	@Override
 	public List<Usuario> obtener() throws ClassException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            Criteria criteria = session.createCriteria(Usuario.class);
+            
+            usuarios = criteria.list();
+            
+        }catch(HibernateException e){
+            throw new ClassException(e);
+        }finally{
+        	session.close();
+        }
+        
+        return usuarios;
 	}
 
 	@Override
 	public Usuario obtenerUsuario(String login) throws ClassException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Usuario usuario = null;
+        Session session = null;
+        try{
+            session = this.getSessionFactory().getCurrentSession();
+            
+            usuario = (Usuario)session.load(Usuario.class, login);
+            
+        }catch(HibernateException e){
+            throw new ClassException(e);
+        }finally{
+            session.close();
+        }
+        
+        return usuario;
 	}
 
 }
